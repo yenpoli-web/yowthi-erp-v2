09db411c07a43d9d668ae549c11a7a2587a51f0e
YowThi ERP Dev v3 <yowthi-erp-dev-v3@local.invalid>
2026-09-02 14:33:15 +0700

    test: validate outsourced supply detail persistence

--- src/YowThi.Erp.Infrastructure/Persistence/DependencyInjection/PersistenceServiceCollectionExtensions.cs
+++ src/YowThi.Erp.Infrastructure/Persistence/DependencyInjection/PersistenceServiceCollectionExtensions.cs
@@ -2,9 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using YowThi.Erp.Application.Common.Idempotency;
 using YowThi.Erp.Application.Common.Transactions;
+using YowThi.Erp.Application.Outsourced;
 using YowThi.Erp.Application.Procurement;
 using YowThi.Erp.Infrastructure.Persistence.Concurrency;
 using YowThi.Erp.Infrastructure.Persistence.Idempotency;
+using YowThi.Erp.Infrastructure.Persistence.Outsourced;
 using YowThi.Erp.Infrastructure.Persistence.Procurement;
 using YowThi.Erp.Infrastructure.Persistence.Transactions;
 
@@ -23,6 +25,7 @@ public static class PersistenceServiceCollectionExtensions
         services.AddSingleton<ICommandRequestHasher, Sha256CommandRequestHasher>();
         services.AddScoped<ICommandTransactionRunner, EfCommandTransactionRunner>();
         services.AddScoped<IConfirmProcurementEntryExecutor, PostgreSqlConfirmProcurementEntryExecutor>();
+        services.AddScoped<IConfirmOutsourcedSupplyDetailExecutor, PostgreSqlConfirmOutsourcedSupplyDetailExecutor>();
         services.AddSingleton<RowVersionSaveChangesInterceptor>();
 
         services.AddDbContext<ErpDbContext>((serviceProvider, options) =>
